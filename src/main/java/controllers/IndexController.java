28017961bed1cd16643f7b26a1add05ca4c1b1aa
unknown <maoyeqin@hikvision.com>
2018-09-13 11:29:41 +0800

    modify controller

--- src/main/java/controllers/IndexController.java
+++ src/main/java/controllers/IndexController.java
@@ -14,7 +14,7 @@ public class IndexController {
 
     @GetMapping("/thead")
     String index2(){
-        int threadNum=2;
+        int threadNum=  2;
         String startDay="20180506";
         String endDay="20180808";
         int daynums=30;
@@ -24,6 +24,18 @@ public class IndexController {
             thread.start();
         }
         return "OK";
+    }
+
+    @GetMapping("/thead2/{threadNum}/{startDay}/{endDay}")
+    String index3(@PathVariable("threadNum")  String StrThreadNum,@PathVariable("startDay")  String startDay,@PathVariable("endDay")  String endDay){
+        int threadNum=  Integer.valueOf(StrThreadNum).intValue();
+        int daynums=30;
+
+        for(int i=0;i< threadNum; i++) {
+            Thread thread = new Thread(new HbaseSendRunable(startDay, endDay, daynums));
+            thread.start();
+        }
+        return "OK2";
     }
 
 }
