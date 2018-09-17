b9a48b00156c94f59de60dda9855f2201d3720e9
margaretmm <myq927@126.com>
2018-09-17 10:14:10 +0800

    Update HbaseSendRunable.java

--- src/main/java/runnable/HbaseSendRunable.java
+++ src/main/java/runnable/HbaseSendRunable.java
@@ -59,6 +59,9 @@ public class HbaseSendRunable implements Runnable  {
         ArrayList<Thread> aList=new ArrayList<Thread>();
         ArrayList<KeyValueForDate> dateList=SplitDateByThreadNum(startDay,endDay,threadNum);
         for(int i=0;i< threadNum; i++) {
+            if (i== dateList.size()){
+                    break;
+            }
             iStart=dateList.get(i).getStartDate();
             iEnd=dateList.get(i).getEndDate();
             iDaynums= daycount(iStart,iEnd);
